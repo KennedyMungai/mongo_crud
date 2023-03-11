6bc81aeb0939a9be0f9ee4ace0450855fa50c604
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-11 12:44:21 +0300

    Added the user and event routes

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ from routes.events import event_router
 import uvicorn
 
 app = FastAPI()
+
+# Register routes
+app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
