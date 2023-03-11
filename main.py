acdcd638923f3094432291718a3d4c050aabfa7b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-11 12:39:09 +0300

    Added a bunch of imports

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 """The entrypoint to the project"""
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from database.connection import conn
+
+from routes.users import user_router
+from routes.events import event_router
+
+import uvicorn
