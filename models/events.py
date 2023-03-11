4358d7462d0280855a51f3b29c1847e180d1bbb2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-11 12:11:00 +0300

    Created the Event class

--- models/events.py
+++ models/events.py
@@ -1,3 +1,11 @@
 """Created a model for the events"""
 from sqlmodel import JSON, SQLModel, Field, Column
 from typing import Optional, List
+
+
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    title: str
+    image: str
+    description: str
+    tags: List[str] = Field(sa_column=Column(JSON))
