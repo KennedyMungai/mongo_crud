d88b10a08c66c574e7adf970aef583f4a8742933
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-11 12:35:51 +0300

    Added connection variables to the file

--- database/connection.py
+++ database/connection.py
@@ -1,1 +1,10 @@
 """This script hand;es database connections"""
+from sqlmodel import SQLModel, Session, create_engine
+from models.events import Event
+
+
+database_file = "planner.db"
+database_connection_string = f"sqlite:///{database_file}"
+connection_args = {"check_same_thread": False}
+engine_url = create_engine(database_connection_string,
+                           echo=True, connection_args=connection_args)
