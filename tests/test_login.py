e23279d2d0f594855bef57957c0b9ade5ac892d2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 17:18:55 +0300

    Defined the sign new user test which is marked asynchronous

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,11 @@
 """A file containing the tests for the login"""
 import httpx
 import pytest
+
+
+@pytest.mark.asyncio
+async def test_sign_new_user(default_client: httpx.AsyncClient) -> None:
+    payload = {
+        "email": "test@user.com",
+        "password": "testpassword"
+    }
