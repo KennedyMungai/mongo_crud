96b2f10a289d8289cbcf79ba96954b352e88f55e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 06:48:14 +0300

    Deleted all the previos code added a bunch of new imports to the python script

--- database/connection.py
+++ database/connection.py
@@ -1,21 +1,5 @@
-"""This script hand;es database connections"""
-from sqlmodel import SQLModel, Session, create_engine
-from models.events import Event
+from typing import Optional
 
-
-database_file = "planner.db"
-database_connection_string = f"sqlite:///{database_file}"
-connection_args = {"check_same_thread": False}
-engine_url = create_engine(database_connection_string,
-                           echo=True, connection_args=connection_args)
-
-
-def conn():
-    """Connection function"""
-    SQLModel.metadata.create_all(engine_url)
-
-
-def get_session():
-    """Session function"""
-    with Session(engine_url) as session:
-        yield session
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseSettings
