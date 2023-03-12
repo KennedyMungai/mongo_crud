2db9e68f6f7804a087cad745ef2b4e4831922d00
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 16:02:43 +0300

    Defined the event_lppo fixture if session scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,10 @@ from database.connection import Settings
 from main import app
 from models.events import Event
 from models.users import User
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
