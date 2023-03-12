e786b3d5b326c09d76f9ec72e376ab21cd9c8df8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 06:52:31 +0300

    Created the settings class

--- database/connection.py
+++ database/connection.py
@@ -3,3 +3,25 @@ from typing import Optional
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    """The settings for connecting to the Mongo DB
+
+    Args:
+        BaseSettings (Class): The parent class
+    """
+    DATABASE_URL: Optional[str] = None
+
+    async def initialize_database(self):
+        """Initialize the database"""
+        client = AsyncIOMotorClient(self.DATABASE_URL)
+
+        await init_beanie(
+            database=client.get_default_database(),
+            document_models=[]
+        )
+
+    class Config:
+        """The configuration for the settings class"""
+        env_file = ".env"
