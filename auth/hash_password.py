54cd1ea8413e204e8f2e86ea3af92c4254b470bc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 10:26:05 +0300

    Created the create_hash function inside teh ashPassword class

--- auth/hash_password.py
+++ auth/hash_password.py
@@ -3,3 +3,8 @@ from passlib.context import CryptContext
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class HashPassword:
+    def create_hash(self, password: str):
+        return pwd_context.hash(password)
