91e711bbdc614c6ffa4645e993181d676048cdbd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 09:55:52 +0300

    Defined the user_signin endpoint

--- routes/users.py
+++ routes/users.py
@@ -32,3 +32,30 @@ async def sign_user_up(user: User) -> dict:
     await user_database.save(user)
 
     return {"Message": "User created successfully"}
+
+
+@user_router.post("/signin")
+async def sign_user_in(user: UserSignIn) -> dict:
+    """The user sign in endpoint
+
+    Args:
+        user (UserSignIn): The sign in data for the user
+
+    Raises:
+        HTTPException: Exception raised incase of wrong email
+        HTTPException: Exception raised incase of wrong password
+
+    Returns:
+        dict: A message to show successful data execution
+    """
+    user_exist = await User.find_one(User.email == user.email)
+
+    if not user_exist:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="The user with the provided email does not exist")
+
+    if user_exist.password == user.password:
+        return {"Message": "User signed in successfully"}
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
