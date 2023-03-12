7113643f0a4025f5deb609f7b468f7fb128d1e27
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 17:36:36 +0300

    Created a fixture that mock inserts events to the database

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,3 +9,19 @@ from models.events import Event
 @pytest.fixture(scope="module")
 async def access_token() -> str:
     return create_access_token("test@user.com")
+
+
+@pytest.fixture(scope="module")
+async def mock_event() -> Event:
+    new_event = Event(
+        creator="test@user.com",
+        title="Last time that I checc'd",
+        image="https://linktomyimage.com/image.png",
+        description="Nipsey Hussle hustling in a nipsey way",
+        tags=["crip", "hiphop", "blood"],
+        location="Google Meet"
+    )
+
+    await Event.insert-one(new_event)
+
+    yield new_event
