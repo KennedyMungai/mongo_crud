c9df14a51b8957e8c5f680c87b5946f792057367
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 14:59:40 +0300

    Imported verify_access_token from jwt_handler

--- auth/authenticate.py
+++ auth/authenticate.py
@@ -1,3 +1,5 @@
 """The script that contains the authentication logic"""
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+
+from auth.jwt_handler import verify_access_token
