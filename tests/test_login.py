67e6a17c571f8c07096777125ffdf0cc19dc350b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 17:20:14 +0300

    Added the headers and the test_response objects

--- tests/test_login.py
+++ tests/test_login.py
@@ -9,3 +9,12 @@ async def test_sign_new_user(default_client: httpx.AsyncClient) -> None:
         "email": "test@user.com",
         "password": "testpassword"
     }
+
+    headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    test_response = {
+        "message": "User created successfully"
+    }
