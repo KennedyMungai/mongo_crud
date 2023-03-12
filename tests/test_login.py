85714d0d8c8098a32af7a85a649135bef3dc93fe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 17:23:26 +0300

    Wrote some assertion statements for the code

--- tests/test_login.py
+++ tests/test_login.py
@@ -18,3 +18,8 @@ async def test_sign_new_user(default_client: httpx.AsyncClient) -> None:
     test_response = {
         "message": "User created successfully"
     }
+
+    response = await default_client.post("/user/signup", json=payload, headers=headers)
+
+    assert response.status_code == 200
+    assert response.json == test_response
