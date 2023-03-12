858507ff50ec232c02123faefa088abc67fe7e0f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 16:00:36 +0300

    Added a bunch of imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,10 @@
 """The umbrella test for the entire application"""
+import asyncio
+
+import httpx
+import pytest
+
+from database.connection import Settings
+from main import app
+from models.events import Event
+from models.users import User
