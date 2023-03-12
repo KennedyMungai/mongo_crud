73a26f4e8943a777edf6ee6891010117c0a124e2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 10:47:33 +0300

    Defined the create_access_token function

--- auth/jwt_handler.py
+++ auth/jwt_handler.py
@@ -9,3 +9,22 @@ from database.database import Settings
 
 
 settings = Settings()
+
+
+def create_access_token(user: str) -> str:
+    """The function to create access tokens
+
+    Args:
+        user (str): Not sure
+
+    Returns:
+        str: The token
+    """
+    payload = {
+        "user": user,
+        "expires": time.time() + 3600
+    }
+
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
+
+    return token
