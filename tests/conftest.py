67120cdb17034ebd7d35b9dcebb2e1bfdbadb9cf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 16:04:22 +0300

    Defined the function to initialize the database

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,3 +15,10 @@ def event_loop():
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
+
+
+async def init_db():
+    test_settings = Settings()
+    test_settings.DATABASE_URL = "mongodb://localhost:27017/testdb"
+
+    await test_settings.initialize_database()
