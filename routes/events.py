17e9df284ed0ef3e52f0da3edcfa996017922aa2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 09:30:57 +0300

    Defined a function to retrieve all events

--- routes/events.py
+++ routes/events.py
@@ -11,3 +11,14 @@ from models.events import Event
 event_database = Database(Event)
 
 event_router = APIRouter(tags=["Events"])
+
+
+@event_router.get("/", response_model=List[Event])
+async def retrieve_all_events() -> List[Event]:
+    """The endpoint to retrieve all events
+
+    Returns:
+        List[Event]: All events output as a List
+    """
+    events = await event_database.get_all()
+    return events
