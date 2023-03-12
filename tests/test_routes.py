59eb3ea3b25b71dd0b2645a539ebe5ade9eb8f73
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 17:32:11 +0300

    Created the access_token fixture

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -4,3 +4,8 @@ import pytest
 
 from auth.jwt_handler import create_access_token
 from models.events import Event
+
+
+@pytest.fixture(scope="module")
+async def access_token() -> str:
+    return create_access_token("test@user.com")
