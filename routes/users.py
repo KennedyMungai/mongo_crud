08ff03fda7c9ae1ba537c08080415f2ff37087cc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 15:17:56 +0300

    Replaced the User Signin import with Token response

--- routes/users.py
+++ routes/users.py
@@ -5,7 +5,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from auth.hash_password import HashPassword
 from auth.jwt_handler import create_access_token
 from database.connection import Database
-from models.users import User, UserSignIn
+from models.users import User, TokenResponse
 
 user_router = APIRouter(tags=["User"])
 
