3ea9fbef051400603f78b1ca45d42ba2d851f3d3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 07:04:47 +0300

    Created the EventUpdate class

--- models/events.py
+++ models/events.py
@@ -2,6 +2,8 @@ from typing import List, Optional
 
 from beanie import Document
 
+from pydantic import BaseModel
+
 
 class Event(Document):
     """The template for the Event document
@@ -30,3 +32,28 @@ class Event(Document):
     class Settings:
         """The settings"""
         name = "events"
+
+
+class EventUpdate(BaseModel):
+    """The event update data template
+
+    Args:
+        BaseModel (Class): The parent class
+    """
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+
+    class Config:
+        """Configuration for the EventUpdate class"""
+        schema_extra = {
+            "example": {
+                "title": "FastAPI book launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
