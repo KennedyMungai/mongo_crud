c5b551380b5dad0bcf6b5546c7a43b93684da680
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 07:10:10 +0300

    Created the User class

--- models/users.py
+++ models/users.py
@@ -4,3 +4,28 @@ from beanie import Document, Link
 from pydantic import BaseModel, EmailStr
 
 from models.events import Event
+
+
+class User(Document):
+    """The template for teh User class
+
+    Args:
+        Document (_type_): _description_
+    """
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]]
+
+    class Settings:
+        """The settings subclass for the User class"""
+        name = "users"
+
+    class Config:
+        """The configuration subclass"""
+        schema_extra = {
+            "example": {
+                "email": "fast@api.com",
+                "password": "strong!!!",
+                "events": []
+            }
+        }
