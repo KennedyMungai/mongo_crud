d3200c7bf2eada0c6ec45ade41060c0ae5f38ec3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 10:55:06 +0300

    Added a docstring to the verify_access_token function

--- auth/jwt_handler.py
+++ auth/jwt_handler.py
@@ -28,3 +28,36 @@ def create_access_token(user: str) -> str:
     token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
 
     return token
+
+
+def verify_access_token(token: str) -> dict:
+    """A function to verify the access token
+
+    Args:
+        token (str): The access token
+
+    Raises:
+        HTTPException: An exception for when there is no access token
+        HTTPException: An exception for when the token has expired
+        HTTPException: An exception for when the token is invalid
+
+    Returns:
+        dict: _description_
+    """
+    try:
+        data = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+
+        expire = data.get("expires")
+
+        if expire is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="No access token supplied")
+
+        if datetime.utcnow() > datetime.utcfromtimestamp(expire):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Token expired")
+
+        return data
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid token")
