ac93be5e49368b36d3e64b48eadccd13f2d74476
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-12 15:48:06 +0300

    Defined the operations to be tested

--- tests/test_arithmetic_operations.py
+++ tests/test_arithmetic_operations.py
@@ -1,1 +1,17 @@
 """Simple arithmetic operations test"""
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    return a - b
+
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+def divide(a: int, b: int) -> int:
+    return a / b
